d62d39df85dd6d2b1e0fd71ca841d6e399940824
bellettif <bellettif@gmail.com>
2014-09-26 14:37:35 -0700

    Tried simple buckets

--- Python_wksp/NGSIM_freeway/data_input/traj_data.py
+++ Python_wksp/NGSIM_freeway/data_input/traj_data.py
@@ -6,47 +6,41 @@ Created on Sep 23, 2014
 
 import csv
 import pandas
+import numpy as np
 
 from matplotlib import pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.cm as cmx
 
 periods = ['0750am-0805am',
            '0805am-0820am',
            '0820am-0835am']
 
-period = periods[0]
 
-target_dir = '/Users/francois/PATH_data/ARZ Datasets/US-101/vehicle-trajectory-data/'
+target_dir = '/Users/cusgadmin/projectARZ/US-101/vehicle-trajectory-data/'
 
-file_path = '%s%s/trajectories-%s.csv' % (target_dir, period, period)
+foot_to_meter = 0.3048
 
-data = pandas.read_csv(file_path)
-
-min_t = float(data['time_since_epoch_ms'].min())
-max_t = float(data['time_since_epoch_ms'].max())
-
-min_x = float(data['global_x'].min())
-max_x = float(data['global_x'].max())
-
-print min_t
-print max_t
-
-print min_x
-print max_x
-
-id_values = list(set(data[data['veh_class'] == 2]['vehicule_ID'].values))
-
-for id_value in id_values:
-    sub_selection = data[data['vehicule_ID'] == id_value]
-    t_values = sub_selection['time_since_epoch_ms'].values
-    t_values = (t_values - min_t) / (max_t - min_t)
-    x_values = sub_selection['global_x'].values
-    x_values = (x_values - min_x) / (max_x - min_x)
-    plt.plot(x_values, t_values, lw = 0.1)
-plt.title('NGSIM traj. US-101')
-plt.xlabel('Global x position')
-plt.ylabel('t')
-plt.savefig('US-101_traj_subset_2.png', dpi = 600)
-plt.close()
+for period in periods:
+    file_path = '%s%s/trajectories-%s.csv' % (target_dir, period, period)
+    data = pandas.read_csv(file_path)
+    #
+    min_t = float(data['time_since_epoch_ms'].min())
+    min_v = float(data['veh_v'].min())
+    max_v = float(data['veh_v'].max())
+    print 'v belongs to (%f, %f)' % (min_v, max_v)
+    #
+    t_values = (data['time_since_epoch_ms'].values - min_t) / float(1000)
+    x_values = data['global_x'].values * foot_to_meter
+    v_values = data['veh_v'].values * foot_to_meter
+    sc = plt.scatter(x_values, t_values, s = 0.1, alpha = 0.2, 
+                     c = v_values , lw = 0, cmap = plt.get_cmap('hot'))
+    plt.colorbar(sc)
+    plt.title('NGSIM traj. US-101 (color is speed m/s)')
+    plt.xlabel('Global x position (meters)')
+    plt.ylabel('t (seconds)')
+    plt.savefig('US-101_traj_subset_%s.png' % period, dpi = 600)
+    plt.close()
 
 
 
